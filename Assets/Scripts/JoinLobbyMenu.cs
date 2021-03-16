4d7e1c9bb1dbf67a3789f6f90bc86c85c78cabf5
ztlacer <reitlacer@gmail.com>
2021-03-15 23:17:05 -0400

    Added Lobby UI which connects two players and puts them in the same scene. Discrepancies between the different players need to be sorted out.

---
+++ Assets/Scripts/JoinLobbyMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class JoinLobbyMenu : MonoBehaviour
+{
+    [SerializeField] private NetManager networkManager = null;
+
+    [Header("UI")]
+    [SerializeField] private GameObject landingPagePanel = null;
+    [SerializeField] private TMP_InputField ipAddressInputField = null;
+    [SerializeField] private Button joinButton = null;
+
+    private void OnEnable()
+    {
+        NetManager.OnClientConnected += HandleClientConnected;
+        NetManager.OnClientDisconnected += HandleClientDisconnected;
+    }
+
+    private void OnDisable()
+    {
+        NetManager.OnClientConnected -= HandleClientConnected;
+        NetManager.OnClientDisconnected -= HandleClientDisconnected;
+    }
+
+    public void JoinLobby()
+    {
+        string ipAddress = ipAddressInputField.text;
+        networkManager.networkAddress = ipAddress;
+        networkManager.StartClient();
+
+        joinButton.interactable = false;
+    }
+
+    private void HandleClientConnected()
+    {
+        joinButton.interactable = true;
+
+        gameObject.SetActive(false);
+        landingPagePanel.SetActive(false);
+    }
+
+    private void HandleClientDisconnected()
+    {
+        joinButton.interactable = true;
+    }
+}
