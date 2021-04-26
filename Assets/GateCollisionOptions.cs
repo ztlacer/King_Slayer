dee31251b674fdcefd821808069385b7812d6a27
Cristina Wong <wildshadow14@gmail.com>
2021-04-26 12:31:26 -0400

    Gate activates on key bind.

---
+++ Assets/GateCollisionOptions.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateCollisionOptions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Activate()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void deActivate()
+    {
+        gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/New Puzzle/SingleGateHandler.cs
+++ Assets/Scripts/New Puzzle/SingleGateHandler.cs
@@ -5,11 +5,50 @@ using UnityEngine;
 public class SingleGateHandler : MonoBehaviour
 {
     public DisplayPuzzle puzzleDisplay;
+    public GateCollisionOptions gateCollision;
     public int gateId;
+    public bool active;
 
-    void OnMouseDown()
+    /*void OnMouseDown()
     {
         puzzleDisplay.Activate();
         puzzleDisplay.ScrambleByGate(gateId);
+    }*/
+
+    void Start()
+    {
+        active = false;
+    }
+
+    private void Update()
+    {
+        if (active)
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                print("E was pressed!");
+                gateCollision.deActivate();
+                puzzleDisplay.Activate();
+                puzzleDisplay.ScrambleByGate(gateId);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "gateCollider")
+        {
+            gateCollision.Activate();
+            active = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "gateCollider")
+        {
+            gateCollision.deActivate();
+            active = false;
+        }
     }
 }
