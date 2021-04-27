dfdd591f96c15d7be27c066f09fc29c2e4706394
Maxx Estes <maxxestest@gmail.com>
2021-04-27 16:23:33 -0400

    Changes to display and inventories
    Players now see their armor, cloak, and sword in the bottom of the inventory. They can also now use health items to restore HP.

--- Assets/Scripts/Inventory/DisplayInventory.cs
+++ Assets/Scripts/Inventory/DisplayInventory.cs
@@ -20,6 +20,8 @@ public class DisplayInventory : MonoBehaviour
     public int Y_SPACE_BETWEEN_ITEMS;
     public int X_GOLD_START;
     public int Y_GOLD_START;
+    public int X_EQUIPMENT_START;
+    public int Y_EQUIPMENT_START;
     int localGoldAmount;
     Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject>();
 
@@ -40,6 +42,10 @@ public class DisplayInventory : MonoBehaviour
     public void clearItems()
     {
         itemsDisplayed.Clear();
+        foreach (Transform child in transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
     }
     public void CreateDisplay()
     {
@@ -47,11 +53,15 @@ public class DisplayInventory : MonoBehaviour
             gold.GetComponent<RectTransform>().localPosition = new Vector3(X_GOLD_START, Y_GOLD_START, 0f);
             gold.GetComponentInChildren<TextMeshProUGUI>().text = "Gold: " + playerStats.goldAmount.ToString();
             localGoldAmount = playerStats.goldAmount;
-
+        int numInvItems = 0;
         for (int i = 0; i < inventory.Container.Count; i++)
         {
+            if (inventory.Container[i].item.name == "Armor" || inventory.Container[i].item.name == "Sword" || inventory.Container[i].item.name == "Cloak")
+            {
+                numInvItems++;
+            }
             var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
-            obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+            obj.GetComponent<RectTransform>().localPosition = GetPosition(i - numInvItems);
             obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
             obj.GetComponentInChildren<TextMeshProUGUI>().color = Color.black;
             if (i == inventory.currentContainerLook)
@@ -74,10 +84,15 @@ public class DisplayInventory : MonoBehaviour
             gold.GetComponentInChildren<TextMeshProUGUI>().text = "Gold: " + playerStats.goldAmount.ToString();
             localGoldAmount = playerStats.goldAmount;
         }
+        int numInvItems = 0;
         for (int i = 0; i < inventory.Container.Count; i++)
         {
             if (itemsDisplayed.ContainsKey(inventory.Container[i]))
             {
+                if (inventory.Container[i].item.name == "Armor" || inventory.Container[i].item.name == "Sword" || inventory.Container[i].item.name == "Cloak")
+                {
+                    numInvItems++;
+                }
                 itemsDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
                 itemsDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().color = Color.black;
                 if (i == inventory.currentContainerLook)
@@ -89,9 +104,26 @@ public class DisplayInventory : MonoBehaviour
             else
             {
                 var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
-                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
                 obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
                 itemsDisplayed.Add(inventory.Container[i], obj);
+                if (inventory.Container[i].item.name == "Armor")
+                {
+                    obj.GetComponent<RectTransform>().localPosition = new Vector3(X_EQUIPMENT_START, Y_EQUIPMENT_START, 0f);
+                    numInvItems++;
+                }
+                else if (inventory.Container[i].item.name == "Sword")
+                {
+                    obj.GetComponent<RectTransform>().localPosition = new Vector3(X_EQUIPMENT_START + 100, Y_EQUIPMENT_START, 0f);
+                    numInvItems++;
+                }
+                else if (inventory.Container[i].item.name == "Cloak")
+                {
+                    obj.GetComponent<RectTransform>().localPosition = new Vector3(X_EQUIPMENT_START + 200, Y_EQUIPMENT_START, 0f);
+                    numInvItems++;
+                }
+                else {
+                    obj.GetComponent<RectTransform>().localPosition = GetPosition(i - numInvItems);
+                }
                 if (i == inventory.currentContainerLook)
                 {
                     obj.GetComponentInChildren<TextMeshProUGUI>().outlineColor = new Color32(0, 0, 0, 255);
